7d8dba57b3aadb839edd913dcd010b0597f0255a
Nero7991 <orencollaco97@gmail.com>
2017-08-18 21:05:51 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ATmega328P-nRF24L01-RX.cpp
+ *
+ * Created: 8/18/2017 9:03:42 PM
+ * Author : orencollaco
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
